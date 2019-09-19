95ce33e7ff5c7098cfa03782ad0b54853a1dd1ca
Bitha Elias <bitha.jordan@gmail.com>
2019-09-19 13:31:34 -0400

    it works now

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let buttonSeven = document.querySelector(".btn-7")
 console.log(buttonSeven)
 buttonSeven.addEventListener('click', function(){ 
-    output.innerText = "7"
+    output.innerText += "7"
     console.log(buttonSeven)
 })
 let output = document.querySelector(".output")
@@ -10,58 +10,74 @@ console.log(output.innerText)
 let buttonEight = document.querySelector(".btn-8")
 console.log(buttonEight)
 buttonEight.addEventListener('click', function(){
-    output.innerText = "8"
+    output.innerText += "8"
     console.log(buttonEight)
 })
 let buttonNine = document.querySelector(".btn-9")
 buttonNine.addEventListener('click', function(){
-    output.innerText = "9"
+    output.innerText += "9"
 })
 let buttonFour = document.querySelector(".btn-4")
 buttonFour.addEventListener('click', function(){
-    output.innerText = "4"
+    output.innerText += "4"
 })
 let buttonFive = document.querySelector(".btn-5")
 buttonFive.addEventListener('click', function(){
-    output.innerText = "5"
+    output.innerText += "5"
 })
 let buttonSix = document.querySelector(".btn-6")
 buttonSix.addEventListener('click', function(){
-    output.innerText = "6"
+    output.innerText += "6"
 })
 let buttonOne = document.querySelector(".btn-1")
 buttonOne.addEventListener('click', function(){
-    output.innerText = "1"
+    output.innerText += "1"
 })
 let buttonTwo = document.querySelector(".btn-2")
 buttonTwo.addEventListener('click', function(){
-    output.innerText = "2"
+    output.innerText += "2"
 })
 let buttonThree = document.querySelector(".btn-3")
 buttonThree.addEventListener('click', function(){
-    output.innerText = "3"
+    output.innerText += "3"
 })
 let buttonZero = document.querySelector(".btn-0")
 buttonZero.addEventListener('click', function(){
-    output.innerText = "0"
+    output.innerText += "0"
 })
 let buttonPeriod = document.querySelector(".btn-period")
 buttonPeriod.addEventListener('click', function(){
-    output.innerText = "."
+    output.innerText += "."
 })
 let buttonPlus = document.querySelector(".btn-plus")
 buttonPlus.addEventListener('click', function(){
-    output.innerText = "+"
+    output.innerText += "+"
 })
 let buttonMinus = document.querySelector(".btn-minus")
 buttonMinus.addEventListener('click', function(){
-    output.innerText = "-"
+    output.innerText += "-"
 })
 let buttonTimes = document.querySelector(".btn-x")
 buttonTimes.addEventListener('click', function(){
-    output.innerText = "*"
+    output.innerText += "*"
 })
 let buttonForwardSlash = document.querySelector(".btn-forward-slash")
 buttonForwardSlash.addEventListener('click', function(){
-    output.innerText = "/"
+    output.innerText += "/"
 }) 
+let clearButton = document.querySelector(".btn-C")
+console.log({ clearButton })
+clearButton.addEventListener('click', function() {
+    output.innerText = ""
+})
+
+let equalButton = document.querySelector(".btn-equal")
+    equalButton.addEventListener('click', function(){
+        output.innerText = eval(output.innerText);
+        console.log(eval(output.innerText))
+ })
+
+
+//  for (let output of field.innerText() {
+// //     console.log(field.innerText)
+// // }
